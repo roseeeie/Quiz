a66a3d46b8965ce50ed7c3b8f4768c9af71eb5e2
CodeThales <thalles.agency@gmail.com>
2021-11-26 20:34:24 -0300

    <Create>[Folders in Project]: <Create classes structure in project>
    Models:
    Option
    Player
    PlayerAnswers
    Question
    Quiz
    
    Services:
    OptionService
    PlayerAnswersService
    PlayerService
    QuestionService
    QuizService
    
    Controllers:
    APIBaseController
    OptionController
    PlayersAnswersController
    PlayerController
    QuestionController
    QuizController
    
    Data:
    Context
    SeedDatabase

---
+++ Musical Quiz/Services/QuizService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Musical_Quiz.Services
+{
+    public class QuizService
+    {
+    }
+}
