3a47c3dcdaf6cc97a012dd40e06df3fde58f051e
CodeThales <thalles.agency@gmail.com>
2021-11-26 22:53:38 -0300

    <feat>[seed database]: <seed database to quiz, question and option tables>

--- Musical Quiz/Data/SeedDatabase.cs
+++ Musical Quiz/Data/SeedDatabase.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Musical_Quiz.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,34 @@ namespace Musical_Quiz.Data
 {
     public class SeedDatabase
     {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new Context(
+                serviceProvider.GetRequiredService<DbContextOptions<Context>>()))
+            {
+                if (!context.Quiz.Any())
+                {
+                    context.Quiz.Add(new Quiz { Theme = "Rock" });
+                    context.Quiz.Add(new Quiz { Theme = "Samba" });
+                    context.Quiz.Add(new Quiz { Theme = "Sertanejo" });                    
+                }
+
+                if (!context.Question.Any())
+                {
+                    context.Question.Add(new Question { Quest = "Qual a melhor banda de todos os tempos da última semana?" });
+                    context.Question.Add(new Question { Quest = "Complete a música: Diga aonde você vai, que eu vou:" });
+                    context.Question.Add(new Question { Quest = "Qual famosa dupla sertaneja foi tema de filme e é conhecida como os 2 filhos de Francisco?" });
+                }
+
+                if (!context.Option.Any())
+                {
+                    context.Option.Add(new Question { Alternative = "Titãs", IsCorrect = true });
+                    context.Option.Add(new Question { Alternative = "que eu vou varrendo.", IsCorrect = true });
+                    context.Option.Add(new Question { Alternative = "Zezé de Camargo e Luciano", IsCorrect = true });
+                }
+
+                context.SaveChanges();
+            }
+        }
     }
 }
