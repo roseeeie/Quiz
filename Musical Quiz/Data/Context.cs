4c27c46a2162b20a03302aa0c42dcdbe8627bf48
CodeThales <thalles.agency@gmail.com>
2021-11-26 22:52:23 -0300

    <feat>[context]: <create db set tables>

--- Musical Quiz/Data/Context.cs
+++ Musical Quiz/Data/Context.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Musical_Quiz.Data
 {
-    public class Context
+    public class Context : IdentityDbContext
     {
+        public Context(DbContextOptions<Context> options) : base (options) { }
+
+        public DbSet<Player> Player { get; set; }
+        public DbSet<Quiz> Quiz { get; set; }
+        public DbSet<Question> Question { get; set; }
+        public DbSet<Option> Option { get; set; }
+        public DbSet<PlayerAnswers> PlayerAnswers { get; set; }
     }
 }
