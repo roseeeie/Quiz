d0f1949bd4422406c9066495c1066db23442a8bd
Rosicleia Furst <78262272+roseeeie@users.noreply.github.com>
2021-11-29 21:22:59 -0300

    Model Options

--- Musical Quiz/Models/Options.cs
+++ Musical Quiz/Models/Options.cs
@@ -5,7 +5,12 @@ using System.Threading.Tasks;
 
 namespace Musical_Quiz.Models
 {
-    public class Option
+    public class Options
     {
+        public int Id { get; set; }
+        public int? QuestionsId { get; set; }
+        public string Option { get; set; }
+        public bool IsCorrect { get; set; }
+
     }
 }
