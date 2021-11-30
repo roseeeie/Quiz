ebd32b9a0b5af6d3390c82e5665c4f17d749b62b
roseeeie <rosifurst1234@gmail.com>
2021-11-29 22:05:32 -0300

    Model Question

--- Musical Quiz/Models/Question.cs
+++ Musical Quiz/Models/Question.cs
@@ -7,5 +7,8 @@ namespace Musical_Quiz.Models
 {
     public class Question
     {
+        public int Id { get; set; }
+        public int? QuizId{ get; set; }
+        public string Quest { get; set; }
     }
 }
