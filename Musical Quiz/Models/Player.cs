5ee53b9efac63e4c3e6761593d82f5f16dce58d0
Rosicleia Furst <78262272+roseeeie@users.noreply.github.com>
2021-11-29 21:10:32 -0300

    Models
    Models

--- Musical Quiz/Models/Player.cs
+++ Musical Quiz/Models/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,17 @@ namespace Musical_Quiz.Models
 {
     public class Player
     {
-            
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public int Points { get; set; }
+
+        public DateTime? created { get; set; }
+        public DateTime? updated { get; set; }
+        public string updatedById { get; set; }
+        public IdentityUser updatedBy { get; set; }
+        public string createdById { get; set; }
+        public IdentityUser createdBy { get; set; }
+
+
     }
 }
